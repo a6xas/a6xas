b5c4a7cedf71a587b6d00e750df1bb54b69c0443
a6xas <142031508+a6xas@users.noreply.github.com>
2023-08-28 00:27:30 +0000

    modified:   IDMW/css/webconf.css 	modified:   IDMW/index.html 	modified:   IDMW/js/webconf.js

--- IDMW/js/webconf.js
+++ IDMW/js/webconf.js
@@ -44,3 +44,51 @@ window.onload = function () {
     });
 
 };
+
+(async() => {
+    const renderSpeakers = document.getElementById("renderSpeakers")
+    let txtSpeakers = ""
+    const response = await fetch('${url_base}/conferences/1/speakers')
+    const speakers = await response.json()
+
+    for (const speaker of speakers) {
+        txtSpeakers += `
+        <div class="col-sm-4">
+                    <div class="team-member">
+                        <img class="mx-auto rounded-circle" src="${speaker.foto}" alt="">
+                        <h4>${speaker.nome}</h4>
+                        <p class="text-muted">${speaker.cargo}</p>
+                        <ul class="list-inline social-buttons">`
+        if (speaker.twitter!==null) {
+            txtSpeakers += `
+            <li class="list-inline-item">
+                <a href="${speaker.twitter}" target="_blank">
+                    <i class=fab fa-twitter"></i>
+                </a>
+            </li>`
+        }
+        if (speaker.facebook!==null) {
+            txtSpeakers += `
+            <li class="list-inline-item">
+                <a href="${speaker.facebook}" target="_blank">
+                    <i class=fab fa-facebook"></i>
+                </a>
+            </li>`
+        }
+        if (speaker.linkedin!==null) {
+            txtSpeakers += `
+            <li class="list-inline-item">
+                <a href="${speaker.linkedin}" target="_blank">
+                    <i class=fab fa-linkedin"></i>
+                </a>
+            </li>`
+        }
+        txtSpeakers += `
+                        </ul>
+                    </div>
+                </div>
+                `
+        
+    }
+    renderSpeakers.innerHTML = txtSpeakers
+})()
