a943cf8326017b87ce6c69c01a60471c830c5df0
a6xas <142031508+a6xas@users.noreply.github.com>
2023-09-18 01:08:56 +0000

    modified:   IDMW/admin/js/speakers.js

--- IDMW/admin/js/speakers.js
+++ IDMW/admin/js/speakers.js
@@ -5,27 +5,40 @@ const urlBase = "https://fcawebbook.herokuapp.com"
 const frmSpeaker = document.getElementById("frmSpeakers")
 frmSpeaker.addEventListener("submit", async (event) => {
     event.preventDefault()
-    //pedido html para inserção dos dados do orador
-    const response = await fetch(`${urlBase}/speakers`, {
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        method: "POST",
-        body: `nome=${txtName}&cargo=${txtJob}&foto=${txtPhoto}&bio=${txtBio}`
-    }
-    )
-    const isNewSpeaker = await response.json()
-    const newSpeakerId = response.headers.get("Location")
-    const newUrl = `${urlBase}/conferences/1/speakers/${newSpeakerId}`
+    if(isNew){
+        //pedido html para inserção dos dados do orador
+        const response = await fetch(`${urlBase}/speakers`, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            method: "POST",
+            body: `nome=${txtName}&cargo=${txtJob}&foto=${txtPhoto}&bio=${txtBio}`
+        }
+        )
+        const isNewSpeaker = await response.json()
+        const newSpeakerId = response.headers.get("Location")
+        const newUrl = `${urlBase}/conferences/1/speakers/${newSpeakerId}`
 
-    const response2 = await fetch(newUrl, {
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        method: "POST",
+        const response2 = await fetch(newUrl, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            method: "POST",
+        }
+        )
+        const isNewSpeaker2 = await response2.json()
+    } else {
+        //pedido para actualizar dados
+        const response = await fetch(`${urlBase}/speakers/${txtSpeakerId}`, {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            method: "PUT",
+            body: `nome=${txtName}&cargo=${txtJob}&foto=${txtPhoto}&bio=${txtBio}`
+        }
+        )
+        const newSpeaker = await response.json()
     }
-    )
-    const isNewSpeaker2 = await response2.json()
 }
 )
 
@@ -66,11 +79,58 @@ const renderSpeakers = async () => {
 }
 
 const btnEdit = document.getElementsByClassName("edit")
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+for (let index = 0; index < btnEdit.length; index++) {
+    btnEdit[i].addEventListener("click", ()=>{
+        isNew = false  // verificar  nome da variavel
+        for (const speaker of speakers) {
+            if (speaker.idSpeaker == btnEdit[i].getAttribute("id")){
+                document.getElementById("txtSpeakerId").value = speaker.idSpeaker
+                document.getElementById("txtName").value = speaker.nome
+                document.getElementById("txtJob").value = speaker.cargo
+                document.getElementById("txtPhoto").value = speaker.foto
+                document.getElementById("txtBio").value = speaker.bio
+            }
+        }
+    }
+    )
 }
 
-
+const btnDelete = document.getElementsByClassName("remove")
+    for (let i = 0; i < btnDelete.length; i++) {
+        btnDelete.addEventListener("click", () => {
+            //janela modal para confirmar remoção
+            swal({
+                title: 'Tem a certeza?',
+                text: "Isto é irreversível",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                cancelButtonText: 'Cancelar',
+                confirmButtonText: 'Remover'
+            }).then(async (result) => {
+                if(result.value){
+                    //pedido http para remoção da inscrição
+                    let speakerId = btnDelete[i].getAttribute("id")
+                    try {
+                        const response = await fetch(`${urlBase}/conferences/1/participants/${speakerId}`, {method: "DELETE"})
+                        const isRemoved = await response.json()
+                        swal(
+                            'Remoção de orador', 
+                            isRemoved.message.pt, 
+                            (isRemoved.success) ? 'success' : 'error'
+                        )
+                        renderParticipants()
+                    } catch (err) {
+                        swal({
+                            type: 'error', 
+                            title: 'Remoção de orador',
+                            text: err
+                        })
+                    }
+                }
+            })
+        })
+    }
 
 }
